a791e029ae4d6baca0bc69ab457863edb4a6c609
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2017-06-15 11:25:54 +0200

    deserialisation of ValidationErrorContent should work correctly now

--- src/Narato.ResponseMiddleware.Models/Models/ValidationErrorContent.cs
+++ src/Narato.ResponseMiddleware.Models/Models/ValidationErrorContent.cs
@@ -4,6 +4,12 @@ namespace Narato.ResponseMiddleware.Models.Models
 {
     public class ValidationErrorContent<T>
     {
+        // ctor so Newtonsoft can deserialise a ValidationErrorContent. (the interfaced property otherwise causes it to fail)
+        public ValidationErrorContent(ModelValidationDictionary<T> validationMessages)
+        {
+            ValidationMessages = validationMessages;
+        }
+
         public IModelValidationDictionary<T> ValidationMessages { get; set; }
     }
 }
---
+++ test/Narato.ResponseMiddleware.Models.Test/Deserialization/ValidationErrorContentDeserializationTest.cs
@@ -0,0 +1,23 @@
+﻿using Narato.ResponseMiddleware.Models.Models;
+using Narato.StringExtensions;
+using Xunit;
+
+namespace Narato.ResponseMiddleware.Models.Test.Deserialization
+{
+    public class ValidationErrorContentDeserializationTest
+    {
+        [Fact]
+        public void TestValidationErrorContentCanGetDeserialized()
+        {
+            var json = "{\"validationMessages\":{\"meep\":[\"moop\"]}}";
+
+            var vec = json.FromJson<ValidationErrorContent<string>>();
+            Assert.IsType<ModelValidationDictionary<string>>(vec.ValidationMessages);
+
+            var messages = vec.ValidationMessages as ModelValidationDictionary<string>;
+
+            Assert.Contains("meep", messages.Keys);
+            Assert.Contains("moop", messages["meep"]);
+        }
+    }
+}
